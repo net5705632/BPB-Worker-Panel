2d1f15ebb0065b4591e7561dd85dd3088afdeef4
net5705632 <46392524+net5705632@users.noreply.github.com>
2024-10-30 09:15:37 +0800

    Update worker.js

--- src/worker.js
+++ src/worker.js
@@ -10,7 +10,7 @@ let userID = '89b3cbba-e6ac-485a-9481-976a0415eab9';
 let trojanPassword = `bpb-trojan`;
 
 // https://www.nslookup.io/domains/bpb.yousef.isegaro.com/dns-records/
-const proxyIPs= ['bpb.yousef.isegaro.com'];
+const proxyIPs= ['ts.hpc.tw']; //bpb.yousef.isegaro.com' edgetunnel.anycast.eu.org bestproxy.onecf.eu.org cdn-all.xn--b6gac.eu.org cdn.xn--b6gac.eu.org proxy.xxxxxxxx.tk
 const defaultHttpPorts = ['80', '8080', '2052', '2082', '2086', '2095', '8880'];
 const defaultHttpsPorts = ['443', '8443', '2053', '2083', '2087', '2096'];
 let proxyIP = proxyIPs[Math.floor(Math.random() * proxyIPs.length)];
